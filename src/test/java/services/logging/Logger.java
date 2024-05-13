718bdd6deaac41974f2b36e9331646ba5cf9b468
Ahtisham <ilyasahtsham@gmail.com>
2024-05-14 00:02:18 +0500

    added services classes and packages

--- src/test/java/services/logging/Logger.java
+++ src/test/java/services/logging/Logger.java
@@ -1,20 +1,20 @@
 package services.logging;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+//import org.apache.log4j.LogManager;
+//import org.apache.log4j.Logger;
 
 public class Logger {
-    private static final Logger logger = LogManager.getLogger(Logger.class);
-
-    public static void info(String message) {
-        logger.info(message);
-    }
-
-    public static void error(String message) {
-        logger.error(message);
-    }
-
-    public static void debug(String message) {
-        logger.debug(message);
-    }
+//    private static final Logger logger = LogManager.getLogger(Logger.class);
+//
+//    public static void info(String message) {
+//        logger.info(message);
+//    }
+//
+//    public static void error(String message) {
+//        logger.error(message);
+//    }
+//
+//    public static void debug(String message) {
+//        logger.debug(message);
+//    }
 }
